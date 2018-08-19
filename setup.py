fbd41247eec22269e90c9c4b506477041dcdc2cb
felipessalvatore <felipessalvatore@github.com>
2018-08-19 14:17:52 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='bovespa2csv',
     version='0.1',
-    description='code to parse Bovespa data',
+    description='Code to parse Bovespa data into a csv or excel',
     author='Felipe Salvatore',
-    packages=find_packages(),
+    author_email='felipessalvador@gmail.com',
+    packages=find_packages(exclude=('tests')),
+    install_requires=['pandas>=0.22.0', 'XlsxWriter>=1.0.7'],
+    license='MIT',
     test_suite="tests"
 )
